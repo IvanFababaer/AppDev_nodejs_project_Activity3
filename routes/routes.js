f27365cc46981fd9f3cc8ef418e7b9f7855e4ebf
IvanFababaer <ivanfababaer018@gmail.com>
2024-09-23 11:56:13 +0800

    Initial commit

--- routes/routes.js
+++ routes/routes.js
@@ -3,4 +3,9 @@ const routes = express.Router();
 const IvanController = require('../controller/IvanController');
 routes.get('/' , IvanController.index);
 
+routes.get('/', IvanController.index);
+routes.get('/about', IvanController.about);
+routes.get('/contact', IvanController.contact);
+routes.get('/services', IvanController.services);
+routes.get('/help', IvanController.help);
 module.exports = routes;
